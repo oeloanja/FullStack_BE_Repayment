46e0c8e1f8150faef11a17882bbb234d31e52635
cho <inbrain144@gmail.com>
2024-11-13 17:10:45 +0900

    feat : spring boot 프로젝트 생성(init)

---
+++ src/test/java/com/billit/repayment/RepaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billit.repayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
