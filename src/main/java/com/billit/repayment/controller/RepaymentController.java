8f819955565f90e16d472503d7fae0d868e39a73
platinum57 <sook8986@naver.com>
2024-11-22 17:40:18 +0900

    feat: 타 서비스와의 연동, 미완료

--- src/main/java/com/billit/repayment/controller/RepaymentController.java
+++ src/main/java/com/billit/repayment/controller/RepaymentController.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/repayments")
+@RequestMapping("/api/v1/repayment-service")
 @RequiredArgsConstructor
 public class RepaymentController {
 
